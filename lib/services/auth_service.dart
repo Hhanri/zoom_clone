f3426cad8f182af93932967069df3aa5e383f328
Hanri <henry018@laposte.net>
2022-06-13 17:52:53 +0200

    showSnackBar function, catch error on signin

--- lib/services/auth_service.dart
+++ lib/services/auth_service.dart
@@ -1,12 +1,14 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
 import 'package:google_sign_in/google_sign_in.dart';
+import 'package:zoom_clone/utils/show_snackbar.dart';
 
 class AuthService {
   final FirebaseAuth auth = FirebaseAuth.instance;
   final FirebaseFirestore firestore = FirebaseFirestore.instance;
 
-  Future<bool> signInWithGoogle() async {
+  Future<bool> signInWithGoogle(BuildContext context) async {
     bool res = false;
 
     try {
@@ -30,8 +32,9 @@ class AuthService {
         }
         res = true;
       }
-    } catch(e) {
+    } on FirebaseAuthException catch(e) {
       res = false;
+      showSnackBar(context: context, text: e.message!);
       print(e);
     }
   }
